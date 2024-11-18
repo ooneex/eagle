f753af3843e4baf1f90183b51727a7ec2867f443
fkdiomande <fkdiomande@icloud.com>
2024-11-18 18:35:18 +0100

    feat(database): update decorators and types
    Update database decorators and types for improved type safety and functionality

--- src/database/decorators.ts
+++ src/database/decorators.ts
@@ -1,41 +1,28 @@
-import { DataSource } from 'npm:typeorm';
 import { container } from '../container/Container.ts';
 import { ServiceDecoratorException } from '../service/ServiceDecoratorException.ts';
 
 export const database = () => {
-    return (database: any) => {
-        const name = database.prototype.constructor.name;
-        ensureIsDatabase(name, database);
+  return (database: any) => {
+    const name = database.prototype.constructor.name;
+    ensureIsDatabase(name, database);
 
-        const dataSource = new DataSource({
-            type: database.getType(),
-            url: database.getConnectionUrl(),
-            synchronize: false,
-            logging: database.logging(),
-            entities: database.getEntities(),
-            migrations: database.getMigrations(),
-            subscribers: database.getSubscribers(),
-        });
+    const dataSource = database.getDataSource();
 
-        container.add(name, dataSource.initialize(), {
-            scope: 'database',
-            singleton: true,
-            instance: true,
-        });
-    };
+    container.add(name, dataSource.initialize(), {
+      scope: 'database',
+      singleton: true,
+      instance: true,
+    });
+  };
 };
 
 const ensureIsDatabase = (name: string, database: any) => {
-    if (
-        !name?.endsWith('Database') ||
-        !database.prototype.getType ||
-        !database.prototype.getConnectionUrl ||
-        !database.prototype.getEntities ||
-        !database.prototype.getMigrations ||
-        !database.prototype.logging
-    ) {
-        throw new ServiceDecoratorException(
-            `Database decorator can only be used on database classes. ${name} must end with Database keyword and implement IDatabase interface.`,
-        );
-    }
+  if (
+    !name?.endsWith('Database') ||
+    !database.prototype.getDataSource
+  ) {
+    throw new ServiceDecoratorException(
+      `Database decorator can only be used on database classes. ${name} must end with Database keyword and implement IDatabase interface.`,
+    );
+  }
 };
--- src/database/types.ts
+++ src/database/types.ts
@@ -1,5 +1,18 @@
 import { DataSource } from 'npm:typeorm';
 
 export interface IDatabase {
-    getDataSource(): DataSource;
+  getDataSource(): DataSource;
+}
+
+export interface IEntity {
+  id: string | number;
+}
+
+export interface IRepository<T extends IEntity> {
+  readonly entityManager: DataSource['manager'];
+  get: (id: string | number) => Promise<T | null>;
+  getAll: () => Promise<T[]>;
+  create: (entity: T) => Promise<T>;
+  update: (entity: T) => Promise<T>;
+  delete: (id: string | number) => Promise<void>;
 }
