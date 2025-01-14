e573199ce3ab1919788848c568991372c26bf1e6
fkdiomande <fkdiomande@icloud.com>
2025-01-14 21:44:41 +0200

    Update version to 0.0.32 and introduce eagle.ts for command execution
    - Bumped the version in package.json from 0.0.31 to 0.0.32 to reflect recent updates.
    - Added a new file eagle.ts that imports and executes the Command class, enhancing the command execution flow.
    - Refactored Command.ts to improve command selection logic by incorporating toPascalCase for better formatting and added a separator in the autocomplete suggestions.
    
    This commit enhances the command execution process and updates versioning for better tracking of changes.

---
+++ eagle.ts
@@ -0,0 +1,4 @@
+import { Command } from './src/command/Command.ts';
+
+const command = new Command();
+await command.execute();
--- src/command/Command.ts
+++ src/command/Command.ts
@@ -1,6 +1,6 @@
 import { intro, outro } from '@clack/prompts';
-import { toKebabCase } from '@std/text';
-import autocomplete from 'inquirer-autocomplete-standalone';
+import { toKebabCase, toPascalCase } from '@std/text';
+import autocomplete, { Separator } from 'inquirer-autocomplete-standalone';
 import { bgBrightBlue, bgBrightGreen, black } from './colors.ts';
 import { CommandContainer } from './container.ts';
 import { dispatchCommand } from './dispatchCommand.ts';
@@ -8,19 +8,33 @@ import { dispatchCommand } from './dispatchCommand.ts';
 export class Command {
   public async execute(): Promise<void> {
     const commands = CommandContainer.toJson()
-      .map((c) => ({
+      .map((c: any) => ({
         value: c.name,
-        description: c.name,
+        name: toKebabCase(c.name)
+          .replaceAll('-', ' ')
+          .replace(/^\w/, (c) => c.toUpperCase()),
       }))
       .sort();
 
+    console.info('\n');
+
     const answer = await autocomplete({
       message: 'Select a command',
-      source: async () => commands,
+      source: async (input) => [
+        new Separator(''),
+        ...commands.filter((c) =>
+          c.value.toLowerCase().includes(input?.toLowerCase() ?? ''),
+        ),
+      ],
+      pageSize: 10,
     });
 
+    console.info('\n');
+
     intro(
-      bgBrightGreen(black(`   ${toKebabCase(answer).replace('-', ' ')}   `)),
+      bgBrightGreen(
+        black(`   ${toPascalCase(answer).replaceAll('-', ' ')}   `),
+      ),
     );
 
     await dispatchCommand(answer);
