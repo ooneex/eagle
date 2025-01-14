a14474bfd29074ef585f88a0fa8d8e1034282de8
fkdiomande <fkdiomande@icloud.com>
2025-01-14 19:16:57 +0200

    Refactor build process and update version to 0.0.23
    - Simplified the build script in `build.ts` by removing unnecessary type exports and adjusting the output directory to `./dist`.
    - Updated `package.json` to reflect the new main entry point and types path, changing the version from 0.0.22 to 0.0.23.
    - Modified `tsconfig.types.json` to align with the new output structure.
    - Cleaned up the GitHub Actions workflow by removing the emoji from the publish step for consistency.
    
    This commit enhances the build configuration, streamlines the output structure, and updates the version for better tracking of changes.

--- build.ts
+++ build.ts
@@ -1,11 +1,8 @@
 import { Glob } from 'bun';
-import packageJson from './package.json';
 
 const typesGlob = new Glob('**/*.test.d.ts');
-const typesDir = './dist/types';
+const typesDir = './dist';
 for (const file of typesGlob.scanSync(typesDir)) {
-  // biome-ignore lint/suspicious/noConsoleLog: trust me
-  console.log(`Deleting ${file}`);
   // @ts-ignore
   await Bun.file(`${typesDir}/${file}`).delete();
 }
@@ -15,33 +12,11 @@ try {
   await Bun.file('tsconfig.types.tsbuildinfo').delete();
 } catch (_e) {}
 
-const sourceGlob = new Glob('*/index.ts');
-const sourceFiles: string[] = ['./src/index.ts'];
-const exports: Record<string, { types: string; import: string }> = {
-  '.': {
-    types: './dist/types/index.d.ts',
-    import: './dist/src/index.js',
-  },
-};
-for (const file of sourceGlob.scanSync('./src')) {
-  sourceFiles.push(`./src/${file}`);
-  exports[`./${file.replace('/index.ts', '')}`] = {
-    types: `./dist/types/${file.replace('.ts', '.d.ts')}`,
-    import: `./dist/src/${file.replace('.ts', '.js')}`,
-  };
-}
-
 await Bun.build({
-  entrypoints: sourceFiles,
+  entrypoints: ['./src/index.ts'],
   outdir: './dist',
-  splitting: true,
+  splitting: false,
   minify: false,
   format: 'esm',
   target: 'bun',
-  sourcemap: 'inline',
-  naming: '[dir]/[name].[ext]',
 });
-
-// @ts-ignore: trust me
-packageJson.exports = exports;
-await Bun.write('./package.json', JSON.stringify(packageJson, null, 2));
