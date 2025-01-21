20489795560c8baa67a4cd12c325fce6d2340b6a
fkdiomande <fkdiomande@icloud.com>
2025-01-21 16:19:55 +0200

    Update version to 0.0.64 in package.json and export BadRequestException
    - Bumped the version in package.json from 0.0.63 to 0.0.64 to reflect recent changes.
    - Exported the BadRequestException from src/exception/index.ts, making it available for use in other modules.
    
    This commit updates the versioning and enhances the exception handling capabilities by exporting the BadRequestException.

--- src/exception/index.ts
+++ src/exception/index.ts
@@ -1,3 +1,4 @@
+export { BadRequestException } from './BadRequestException';
 export { Exception } from './Exception';
 export { NotFoundException } from './NotFoundException';
 export * from './types';
